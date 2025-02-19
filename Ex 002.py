1577f57bc5275e6306fab1890f270ca7eaf6f31e
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-02-19 12:48:39 -0300

    Feito o Exercicio 002, de exibir uma mensagem para o Usuario, foi feito de 2 formas o exercicio.

---
+++ Ex 002.py
@@ -0,0 +1,25 @@
+
+
+# Primeira Forma de Fazer o Ex 002 do Curso em Video de Python
+# Respondendo ao Usuario
+print('Primeira Forma de Fazer o Ex 002 do Curso em Video de Python')
+print('############################')
+nome = input('Digite Seu Nome:')
+print('')
+print('############################')
+print('')
+# mensagem = nome
+print(f'Seja-Bem Vindo, {nome}')
+
+
+# Segunda Forma de Fzer o Ex 002 do Curso em Video de Python
+print('')
+print('############################')
+print('Segunda Forma de Fzer o Ex 002 do Curso em Video de Python')
+print('############################')
+nome = input('Digite Seu Nome:')
+print('')
+print('############################')
+print('')
+# mensagem = nome
+print('Seja-Bem Vindo, {}!'.format(nome))
