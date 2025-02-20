60d085f8f6aae2f822134feb65212357dfe1a166
Thiago-Maximo <142333508+Thiago-Maximo@users.noreply.github.com>
2025-02-20 16:38:11 -0300

    Criado o Arquivo da Aula 7 do Curso de Python, Faltando Finalizar a Video Aula, Porém foram adicionados códigos no Arquivo.

---
+++ Aula-07-Operadores_Aritméticos.py
@@ -0,0 +1,44 @@
+# Operadores Aritméticos
+# + Adição
+# - Subtração
+# * Multiplicação
+# / Divisão
+# ** Potenciação
+# // Divisão Inteira
+# % Resto da Divisão
+
+n1 = 5
+n2 = 2
+resultado_adicao = n1 + n2
+resultado_subtracao = n1 - n2
+resultado_multiplicacao = n1 * n2
+resultado_divisao = n1 / n2
+resultado_potencia = n1 ** n2
+resultado_divisao_inteira = n1 // n2
+resultado_resto_divisao = n1 % n2
+print(f'O  Resultado da Adição de {n1} + {n2} é de : {resultado_adicao} ')
+print(f'O  Resultado da Subtração de {n1} + {n2} é de : {resultado_subtracao}')
+print(f'O  Resultado da Multiplicação de {n1} + {n2} é de : {resultado_multiplicacao}')
+print(f'O  Resultado da Divisão de {n1} + {n2} é de : {resultado_divisao}')
+print(f'O  Resultado da Potenciação de {n1} + {n2} é de : {resultado_potencia}')
+print(f'O  Resultado da Divisão Inteira de {n1} + {n2} é de : {resultado_divisao_inteira}')
+print(f'O  Resultado do Resto da Divisão de {n1} + {n2} é de : {resultado_resto_divisao}')
+
+print('')
+print('#############################################################################')
+
+######################### Ordem De Precedência No Python ###############
+# 1 () : Parenteses
+# 2 ** :  Potenciação
+# 3 *, /, //, % : Multplicação, Divisão, Divisão Inteira, Resto da Divisão
+# 4 +, - : Soma e Subtração
+
+print('Ordem de Precedência No Python')
+print('')
+print('1 - () : Parenteses')
+print('2 - ** :  Potenciação')
+print('3 - *, /, //, % : Multplicação, Divisão, Divisão Inteira, Resto da Divisão')
+print('4 - +, - : Soma e Subtração')
+
+print('')
+print('#############################################################################')
