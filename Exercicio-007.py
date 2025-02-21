3fee44878eb295afc2a8c94413a8c27f090346a0
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-02-21 12:37:13 -0300

    Criado o Arquivo do Exercicio 007, exercicio resolvido com sucesso.

---
+++ Exercicio-007.py
@@ -0,0 +1,9 @@
+# Faça um programa que leia duas notas de um aluno, calcule e exiba o resultado
+
+nota1 = float(input('Digite a Primeira Nota: '))
+nota2 = float(input('Digite a Segunda Nota: '))
+
+resultado = (nota1 + nota2)/2
+
+print('')
+print('A Sua Média é de {}'.format(resultado))
