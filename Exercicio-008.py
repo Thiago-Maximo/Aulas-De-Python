8afcbbc783c60bdf64336ee4ca752bdcfffa5c7b
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-02-21 12:48:39 -0300

    Criado o Arquivo do Exercicio 008, exerccicio resolvido com sucesso.

---
+++ Exercicio-008.py
@@ -0,0 +1,8 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
+
+metros = int(input('Digite os Metros: '))
+centimetros = metros * 100
+milimetros = centimetros * 10
+
+print('')
+print(f'Os {metros} Metros em centimetros são {centimetros} e em milimetros são {milimetros}')
