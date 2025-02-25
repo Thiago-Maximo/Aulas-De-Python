dc392d3bed80d89f913f55e8e8483f21749edbbd
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-02-25 14:36:39 -0300

    Exercico-016 criado e resolvido com Sucesso, utilizando a Biblioteca math Desafio: Escreva um programa que leia um numero real e mostre na tela sua parte inteira. EX: 6.127 tem a parte inteira 6

---
+++ Exercico-016.py
@@ -0,0 +1,8 @@
+# Escreva um programa que leia um numero real e mostre na tela sua parte inteira. EX: 6.127 tem a parte inteira 6
+
+import math
+print('Não Utilize virgula, apenas o Ponto . ')
+real = float(input('Digite um Número Real, EX(6.127): '))
+inteiro = math.floor(real)
+
+print(f'O Número {real} tem como sua parte inteira o numero {inteiro}')
