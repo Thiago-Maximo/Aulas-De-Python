442f0d03143fa72e5f14885bdb2e14309bd85b0b
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-02-25 15:24:30 -0300

    Exercicio-020 resolvido com sucesso, utilizado o comando random.shuffle que embaralha os nomes na lista
    utilizado um for para que enquanto ainda tiver nome na lista ele irá exibir.
    
    Desafio:  O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos, faça um programa que leia o nome dos alunos e mostre a ordem sorteada

---
+++ Exercicio-020.py
@@ -0,0 +1,21 @@
+# O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos, faça um programa que leia o nome dos alunos e mostre a ordem sorteada
+import math
+import random  # Importando a biblioteca correta
+
+vetor_nomes = []
+
+# Solicitar os nomes dos alunos
+print('UTILIZE A VIRGULA PARA SEPARAR')
+nomes = input('Digite o nome de seus alunos: ')
+
+# Transformar a string de nomes em uma lista, separando pelos espaços
+vetor_nomes = nomes.split(',')  # Usando a vírgula como separador
+
+# Embaralhando a lista de nomes
+random.shuffle(vetor_nomes)
+
+# Exibindo os nomes em ordem aleatória
+print("Nomes em ordem aleatória:")
+for nome in vetor_nomes:
+    print(nome.strip())  # .strip() remove espaços extras antes ou depois dos nomes
+
