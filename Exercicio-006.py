3a84d8e2795f6a8472cc8769bb52e3fad3d798dd
SIND_EAA <github.eaa@eaa.org.br>
2025-02-25 13:34:46 -0300

    Exercicio-014 resolvido com sucesso. Probelma proposto Escreva um programa que converta uma temperatura digitada em °C para °F.

--- Exercicio-006.py
+++ Exercicio-006.py
@@ -10,7 +10,7 @@ raiz = n1**(1/2)
 
 print('#'*50)
 print('1° Forma de se Fazer, usando Variaveis')
-print(f' O Dobro de {n1} é {dobro} \n o Triplo é {triplo} \n a Raiz Quadrada é {raiz}')
+print(f' O Dobro de {n1} é {dobro} \n o Triplo é {triplo} \n a Raiz Quadrada é {raiz:.2f}')
 
 print('')
 print('#'*50)
@@ -20,5 +20,5 @@ n2 = int(input('Digite Um Número: '))
 
 print('#'*50)
 print('2° Forma de se Fazer, Passando os Parametros no print()')
-print(f' O Dobro de {n2} é {n2*2} \n o Triplo é {n2*3} \n a Raiz Quadrada é {n2**(1/2)}')
+print(f' O Dobro de {n2} é {n2*2} \n o Triplo é {n2*3} \n a Raiz Quadrada é {n2**(1/2):.2f}')
 print('#'*50)
---
+++ Exercicio-014.py
@@ -0,0 +1,5 @@
+# Escreva um programa que converta uma temperatura digitada em °C para °F.
+
+graus_c = float(input('Digite a Temperatura em graus Celsius °C: '))
+graus_f = graus_c * 9/5 + 32
+print(f'A Temperatura em graus °Fahrenheit é de: {graus_f} °F')
