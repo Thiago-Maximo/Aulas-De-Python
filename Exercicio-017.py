c5b4db0b2a96aed9be0609c0cb652abf7d513b66
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-02-25 14:45:11 -0300

    Exercicio-017 resolvido com sucesso utilizando a biblioteca math, função hypot(), que calcula a huipotenusa de um triangulo retangulo. Desafio: Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retangulo, calcule e mostre o comprimento da hipotenusa.

---
+++ Exercicio-017.py
@@ -0,0 +1,6 @@
+# Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triangulo retangulo, calcule e mostre o comprimento da hipotenusa.
+import math
+cateto_oposto = int(input('Digite o valor do cateto oposto: '))
+cateto_adjacente = int(input('Digite o valor do cateto adjacente: '))
+hipotenusa = math.hypot(cateto_oposto,cateto_adjacente)
+print(f'A Hipotenusa é: {hipotenusa}')
