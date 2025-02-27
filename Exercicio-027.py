99c4f54e5d62c74bed19017de90fff21f06406a5
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-02-27 15:31:03 -0300

    Criado o arquivo do Exercicio-027, faltando a resolução. Desafio: # Crie um Programa que leia um nome completo de uma pessoa, mostrando em seguida o primeiro nome e o ultimo nome separados. # EX: Ana Maria de Souza # Primeiro: Ana # Ultimo: Souza

---
+++ Exercicio-027.py
@@ -0,0 +1,4 @@
+# Crie um Programa que leia um nome completo de uma pessoa, mostrando em seguida o primeiro nome e o ultimo nome separados.
+# EX: Ana Maria de Souza
+# Primeiro: Ana
+# Ultimo: Souza
