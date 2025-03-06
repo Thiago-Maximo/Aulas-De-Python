ae0da8283e3ae8f444bf1a4c94167707d2f3f4dd
SIND-EAA <142333508+Thiago-Maximo@users.noreply.github.com>
2025-03-06 15:25:56 -0300

    Resolvido o Exercicio-027 com Sucesso. Desafio: Crie um Programa que leia um nome completo de uma pessoa, mostrando em seguida o primeiro nome e o ultimo nome separados. EX: Ana Maria de Souza Primeiro: Ana Ultimo: Souza

--- Exercicio-027.py
+++ Exercicio-027.py
@@ -2,3 +2,19 @@
 # EX: Ana Maria de Souza
 # Primeiro: Ana
 # Ultimo: Souza
+
+# Solicita o nome completo da pessoa
+nome_completo = input("Digite seu nome completo: ")
+
+# Divide o nome completo em uma lista de palavras
+nome_dividido = nome_completo.split()
+
+# O primeiro nome será o primeiro item da lista
+primeiro_nome = nome_dividido[0]
+
+# O último nome será o último item da lista
+ultimo_nome = nome_dividido[-1]
+
+# Exibe o primeiro e o último nome
+print(f"Primeiro nome: {primeiro_nome}")
+print(f"Último nome: {ultimo_nome}")
