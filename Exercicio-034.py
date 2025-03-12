15815bdbdf70fa12ba81258873c850d6426a297a
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-03-12 15:30:52 -0300

    Terminando o Exercicio-034. Desafio:# Faça Um Programa que Leia 3 Números e moestre qual é o MAIOR e qual é o MENOR

--- Exercicio-034.py
+++ Exercicio-034.py
@@ -1,1 +1,11 @@
 # Faça Um Programa que Leia 3 Números e moestre qual é o MAIOR e qual é o MENOR
+import math
+from re import split
+
+nomes = input('Digite 3 Numeros:')
+
+print(nomes.split())
+
+nomes = input('Digite 3 Numeros:')
+
+print(nomes.split())
