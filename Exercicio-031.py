3d522bbbf51d57605ce2b003ed735dae9f802c51
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-03-12 11:59:20 -0300

    Resolvido com Sucesso Exercicio-031. Desafio: Crie um Programa que leia um numero inteiro e mostre na tela se ele é par ou impar.

--- Exercicio-031.py
+++ Exercicio-031.py
@@ -1,1 +1,8 @@
 # Crie um Programa que leia um numero inteiro e mostre na tela se ele é par ou impar.
+
+numero = int(input('Digite um numero: '))
+
+if numero % 2 == 0:
+    print('O Número Digitado é Par!')
+else:
+    print('O Número Digitado é Impar!')
