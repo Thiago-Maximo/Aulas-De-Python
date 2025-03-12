ac1511ef005908c9b0fd3810ab8a3fb6fbce4dd1
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-03-12 12:04:22 -0300

    Criado o Aqruivo do Exercicio-032. Desafio: # Crie um programa que pergunte a distancia de uma viagem em KM. # Calcule o preço da passgem, cobrando R$ 0,50 por KM para Viagens de até 200Km. # e R$ 0,45 para viagens mais longas.

---
+++ Exercicio-032.py
@@ -0,0 +1,4 @@
+# Crie um programa que pergunte a distancia de uma viagem em KM.
+# Calcule o preço da passgem, cobrando R$ 0,50 por KM para Viagens de até 200Km.
+# e R$ 0,45 para viagens mais longas.
+
