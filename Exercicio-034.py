2e428d79d9e3ad25355a0623352405ee00300a90
Thiago-Maximo <thiagomaximo271@gmail.com>
2025-03-18 15:01:13 -0300

    Exercicio-034 resolvido com Sucesso Desafio: Programa que lê 3 números e mostra o maior e o menor

--- Exercicio-034.py
+++ Exercicio-034.py
@@ -1,11 +1,44 @@
-# Faça Um Programa que Leia 3 Números e moestre qual é o MAIOR e qual é o MENOR
-import math
-from re import split
+# Programa que lê 3 números e mostra o maior e o menor
 
-nomes = input('Digite 3 Numeros:')
+# Solicita a entrada de 3 números
+numeros = input("Digite 3 números separados por espaço: ")
 
-print(nomes.split())
+# Divide a entrada em uma lista de números
+numeros = [float(x) for x in numeros.split()]
 
-nomes = input('Digite 3 Numeros:')
+# Calcula o maior e o menor número
+maior = max(numeros)
+menor = min(numeros)
 
-print(nomes.split())
+# Exibe os resultados
+print(f"O maior número é: {maior}")
+print(f"O menor número é: {menor}")
+
+print('')
+print('2° Forma de se Fazer')
+print('')
+# 2° Forma de se Fazer
+
+# Programa que lê 3 números e mostra o maior e o menor
+
+# Solicita a entrada de 3 números
+n1, n2, n3 = map(float, input("Digite 3 números separados por espaço: ").split())
+
+# Calcula o maior e o menor número
+maior = n1
+menor = n1
+
+# Comparando diretamente os números
+if n2 > maior:
+    maior = n2
+if n3 > maior:
+    maior = n3
+
+if n2 < menor:
+    menor = n2
+if n3 < menor:
+    menor = n3
+
+# Exibe os resultados
+print(f"O maior número é: {maior}")
+print(f"O menor número é: {menor}")
